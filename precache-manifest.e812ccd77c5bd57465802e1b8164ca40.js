4e2ad3cff186655c486ed48659651e6d88386060
Semion Shishlovi <ruddch@yandex-team.ru>
2019-06-11 16:29:29 +0300

    Updates

---
+++ precache-manifest.e812ccd77c5bd57465802e1b8164ca40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "134f8dfbf0efcc3310b4",
+    "url": "/vim-helper/static/css/main.30b27d51.chunk.css"
+  },
+  {
+    "revision": "134f8dfbf0efcc3310b4",
+    "url": "/vim-helper/static/js/main.134f8dfb.chunk.js"
+  },
+  {
+    "revision": "4fbe63a8df5d33ebd89d",
+    "url": "/vim-helper/static/css/1.9e376814.chunk.css"
+  },
+  {
+    "revision": "4fbe63a8df5d33ebd89d",
+    "url": "/vim-helper/static/js/1.4fbe63a8.chunk.js"
+  },
+  {
+    "revision": "bb6522aff08c8b35be97",
+    "url": "/vim-helper/static/js/runtime~main.bb6522af.js"
+  },
+  {
+    "revision": "4a3ffa8ed55981e7621bdad48c2729cd",
+    "url": "/vim-helper/index.html"
+  }
+];
